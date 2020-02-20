f38ec28785a247af74bd955eeacdb909ce88abe0
jimmy <you@example.com>
2020-02-20 21:34:40 +0800

    添加样例

--- intset/intset.go
+++ intset/intset.go
@@ -3,25 +3,34 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	// 定义集合 x, y，并添加整型元素
 	var x, y IntSet
+	x.Add(168)
 	x.Add(1)
-	x.Add(144)
 	x.Add(9)
 	x.Add(88)
+	x.Add(788)
+	x.Add(10000)
 	fmt.Printf("集合x：\t%s\n", x.String())
 
+	y.Add(0)
 	y.Add(9)
-	y.Add(42)
+	y.Add(89)
+	y.Add(168)
+	y.Add(999)
 	fmt.Printf("集合y：\t%s\n", y.String())
 
-	x.UnionWith(&y)
+	x.DifferenceSet(&y)
 	fmt.Printf("xy的并集：\t%s\n", x.String())
 
 	fmt.Printf("xy的并集是否包含9：%t；xy的并集是否包含123：%t", x.Has(9), x.Has(123))
+
+	fmt.Printf("%s", time.Since(start))
 }
 
 // 定义一个无符号的整型集合（小的非负整数）
@@ -51,6 +60,35 @@ func (s *IntSet) UnionWith(t *IntSet) {
 			s.words[i] |= tword
 		} else {
 			s.words = append(s.words, tword)
+		}
+	}
+}
+
+func (s *IntSet) Intersection(t *IntSet) {
+	result := new(IntSet)
+	for i, w := range t.words {
+		if i < len(s.words) {
+			result.words = append(result.words, w&s.words[i])
+		}
+	}
+
+	s.words = result.words
+}
+
+func (s *IntSet) SymmetricDifference(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] ^= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+func (s *IntSet) DifferenceSet(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] &^= tword
 		}
 	}
 }
