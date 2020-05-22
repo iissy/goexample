477590e2acba762f7456357371d82ec136e63332
jimmy <pinbor@iissy.com>
2020-05-22 10:03:37 +0800

    read && write file

--- file/main.go
+++ file/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
 func main() {
-	$END$
+	data := []byte("Hello World!\n")
+	err := ioutil.WriteFile("data1.txt", data, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	read1, _ := ioutil.ReadFile("data1.txt")
+	fmt.Print(string(read1))
+
+	file1, _ := os.Create("data2.txt")
+	defer file1.Close()
+
+	bytes, _ := file1.Write(data)
+	fmt.Printf("Wrote %d bytes to files\n", bytes)
+
+	file2, _ := os.Open("data2.txt")
+	defer file2.Close()
+
+	read2 := make([]byte, len(data))
+	bytes, _ = file2.Read(read2)
+	fmt.Printf("Read %d bytes from file\n", bytes)
+	fmt.Println(string(read2))
 }
