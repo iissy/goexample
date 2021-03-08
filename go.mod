7fabb0c48f71ed9cd2e408b7e7df43f0672575eb
jimmy <pinbor@iissy.com>
2021-03-08 16:41:14 +0800

    分布式 redis锁

--- go.mod
+++ go.mod
@@ -1,18 +1,23 @@
 module goexample
 
-go 1.13
+go 1.16
 
 require (
+	github.com/go-gorp/gorp v2.2.0+incompatible // indirect
+	github.com/go-redis/redis/v8 v8.7.1
+	github.com/go-redsync/redsync/v4 v4.0.4
 	github.com/go-sql-driver/mysql v1.5.0
-	github.com/gogo/protobuf v1.3.1 // indirect
-	github.com/gorilla/websocket v1.4.1 // indirect
-	github.com/graphql-go/graphql v0.7.8
-	github.com/kr/pretty v0.2.0 // indirect
-	github.com/rakyll/statik v0.1.6 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/graphql-go/graphql v0.7.9
+	github.com/kylelemons/godebug v1.1.0 // indirect
+	github.com/lib/pq v1.10.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.6 // indirect
+	github.com/poy/onpar v1.1.2 // indirect
+	github.com/rakyll/statik v0.1.7 // indirect
 	github.com/samsarahq/go v0.0.0-20191220233105-8077c9fbaed5 // indirect
 	github.com/samsarahq/thunder v0.5.0
 	github.com/satori/go.uuid v1.2.0
-	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	github.com/ziutek/mymysql v1.5.4 // indirect
 	gopkg.in/gorp.v2 v2.2.0
 )
---
+++ redsync/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	goredislib "github.com/go-redis/redis/v8"
+	"github.com/go-redsync/redsync/v4"
+	"github.com/go-redsync/redsync/v4/redis/goredis/v8"
+	"time"
+)
+
+func main() {
+	// Create a pool with go-redis (or redigo) which is the pool redisync will
+	// use while communicating with Redis. This can also be any pool that
+	// implements the `redis.Pool` interface.
+	client := goredislib.NewClient(&goredislib.Options{
+		Addr: "192.168.1.234:6379",
+	})
+	pool := goredis.NewPool(client) // or, pool := redigo.NewPool(...)
+
+	// Create an instance of redisync to be used to obtain a mutual exclusion
+	// lock.
+	rs := redsync.New(pool)
+
+	// Obtain a new mutex by using the same name for all instances wanting the
+	// same lock.
+	key := "hello-test-for-lock"
+	val := "b"
+	mutex := rs.NewMutex(key, redsync.WithExpiry(200*time.Second), redsync.WithGenValueFunc(func() (string, error) {
+		return val, nil
+	}))
+
+	// Obtain a lock for our given mutex. After this is successful, no one else
+	// can obtain the same lock (the same mutex name) until we unlock it.
+	if err := mutex.Lock(); err != nil {
+		fmt.Println(err)
+	}
+
+	h := 323
+	fmt.Print(h)
+
+	// Do your work that requires the lock.
+
+	// Release the lock so other processes or threads can obtain a lock.
+	if ok, err := mutex.Unlock(); !ok || err != nil {
+		panic("unlock failed")
+	}
+}
